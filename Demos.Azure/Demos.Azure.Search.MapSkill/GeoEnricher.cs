6f1c7a05a760b6b3f3895a897457c8423b3966cf
Suneet Nangia <suneetnangia@gmail.com>
2019-03-19 20:05:00 +0000

    Update GeoEnricher.cs
    Added spaces between the using directives.

--- Demos.Azure/Demos.Azure.Search.MapSkill/GeoEnricher.cs
+++ Demos.Azure/Demos.Azure.Search.MapSkill/GeoEnricher.cs
@@ -5,6 +5,7 @@ namespace Demos.Azure.Search.MapSkill
     using System.IO;
     using System.Net.Http;
     using System.Threading.Tasks;
+    
 
     using Demos.Azure.Search.MapSkill.MapService;
     using Demos.Azure.Search.MapSkill.SearchSkillInterface;
